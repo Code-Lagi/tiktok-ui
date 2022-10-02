9d1b5ea23acf29aa92d882e964174038679fb97a
Code-Lagi <114864930+Code-Lagi@users.noreply.github.com>
2022-10-02 21:17:39 +0700

    Update App.js
    chỉnh sửa file để test lệnh git pull

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello anh em F8!!
+          Hello anh em F8!! test github xem nào.
+            
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
