96787f547241bd01414a27d2f99b1cd0699ed55c
Code-Lagi <huuthenguyen@gmail.com>
2022-10-02 21:08:52 +0700

    this is my first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello anh em F8.
+          Hello anh em F8!!
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
