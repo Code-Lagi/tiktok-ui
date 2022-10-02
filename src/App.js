ab3826f5847bc9c79e51827eb41ac250803aa8ab
Coder vui ve <codervuive@capitain.com>
2022-10-02 21:05:18 +0700

    this is my first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello anh em F8.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
