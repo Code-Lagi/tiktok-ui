6e9ec60c9193b4b584516bf17632a39209d3e40a
Code-Lagi <huuthenguyen@gmail.com>
2022-10-03 22:11:28 +0700

    Bài 97: Xây dựng UI phần Header #1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
-import { DefaultLayout, HeaderOnly } from '~/components/Layout';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
     return (
