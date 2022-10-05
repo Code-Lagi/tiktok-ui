eade69bb578b7bd0394056a90d29b93b1fa07090
Code-Lagi <huuthenguyen@gmail.com>
2022-10-05 16:22:03 +0700

    Bài 100: Xây dựng UI phần Header #4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faEllipsisVertical,
+    faEarthAsia,
+    faCircleQuestion,
+    faKeyboard,
+} from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
@@ -9,9 +17,24 @@ import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
-
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
 
@@ -60,6 +83,11 @@ function Header() {
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
                 </div>
+                <Menu items={MENU_ITEMS}>
+                    <button className={cx('more-btn')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical} />
+                    </button>
+                </Menu>
             </div>
         </header>
     );
---
+++ src/components/Popper/Menu/index.js
@@ -0,0 +1,33 @@
+import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import styles from './Menu.module.scss';
+import MenuItem from './MenuItem';
+
+const cx = classNames.bind(styles);
+
+function Menu({ children, items = [] }) {
+    const renderItems = () => {
+        return items.map((item, index) => <MenuItem key={index} data={item} />);
+    };
+
+    return (
+        <div>
+            <Tippy
+                interactive
+                delay={[0, 700]}
+                placement="bottom-end"
+                render={(attrs) => (
+                    <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
+                    </div>
+                )}
+            >
+                {children}
+            </Tippy>
+        </div>
+    );
+}
+
+export default Menu;
